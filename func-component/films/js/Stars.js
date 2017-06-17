75fc52af7797d3f54d6b483e8df87836ad4371f5
Vladimir Isaev <info@isaev.in>
2017-06-17 22:49:36 +0300

    Решена задача с рейтингами в фильмах

--- func-component/films/js/Stars.js
+++ func-component/films/js/Stars.js
@@ -1,5 +1,15 @@
 'use strict';
 
-function Stars() {
-  return <ul className="card-body-stars u-clearfix"><li><Star /></li></ul>;
+function Stars({count}) {
+  const starsOutput = [];
+
+  for (let i = 0; i < count; i++) {
+    starsOutput.push(<Star key={i} />);
+  }
+
+  return (
+    <ul className="card-body-stars u-clearfix">
+      { starsOutput }
+    </ul>
+  );
 }
