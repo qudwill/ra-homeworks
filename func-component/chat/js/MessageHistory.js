753bc715203c744622a795aba9c7ec889882ffba
Vladimir Isaev <info@isaev.in>
2017-06-23 22:14:00 +0300

    Рефакторинг кода по рекомендациям наставника

--- func-component/chat/js/MessageHistory.js
+++ func-component/chat/js/MessageHistory.js
@@ -2,23 +2,21 @@
 
 function MessageHistory({ list }) {
   if (list.length > 0) {
-    let messageList = [];
+    const components = {
+      response: Response,
+      message: Message,
+      typing: Typing
+    };
 
     const messages = list.map(item => {
-      const MessageType = 
-        (item.type === 'response') ? Response :
-        (item.type === 'message') ? Message :
-        (item.type === 'typing') ? Typing :
-        null;
-
-      if (MessageType !== null) {
-        messageList.push(<MessageType key={item.id} from={item.from} message={item} />);
-      }
+      const Component = components[item.type];
+      
+      return <Component key={item.id} from={item.from} message={item} />;
     });
 
     return (
       <ul>
-        { messageList }
+        { messages }
       </ul>
     );
   } else {
