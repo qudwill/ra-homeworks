38ded508adba198d4125d4386dfc55383029d24c
Vladimir Isaev <info@isaev.in>
2017-06-23 22:03:47 +0300

    Добавлена проверка атрибута count, переделан императивный цикл

--- func-component/films/js/Stars.js
+++ func-component/films/js/Stars.js
@@ -1,15 +1,21 @@
 'use strict';
 
 function Stars({count}) {
-  const starsOutput = [];
+  if ( (count < 1) || (count > 5) || !Number.isInteger(count) ) {
+    return null;
+  } else {
+    const arr = [];
 
-  for (let i = 0; i < count; i++) {
-    starsOutput.push(<Star key={i} />);
+    for (let i = 0; i < count; i++) {
+      arr.push(i + 1);
+    }
+
+    const starsOutput = arr.map(key => <Star key={key} />);
+
+    return (
+      <ul className="card-body-stars u-clearfix">
+        { starsOutput }
+      </ul>
+    );
   }
-
-  return (
-    <ul className="card-body-stars u-clearfix">
-      { starsOutput }
-    </ul>
-  );
 }
