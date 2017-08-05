2d0f0444a7e680a684b5b98b47510b28223b0946
Vladimir Isaev <info@isaev.in>
2017-08-05 11:37:39 +0300

    Добавление нового дз state

---
+++ state/filter/js/App.js
@@ -0,0 +1,37 @@
+'use strict'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      current: this.props.filters[0],
+      projects: this.props.projects
+    }
+  }
+    
+  selectFilter(filter) {
+    let projects = this.props.projects;
+    
+    if (filter !== 'All') {
+      projects = projects.filter(project => project.category === filter);
+    }
+    
+    this.setState({
+      current: filter,
+      projects: projects
+    });
+  }
+  
+  render() {
+    return (
+      <div>
+        <Toolbar
+          filters={this.props.filters}
+          selected={this.state.current}
+          onSelectFilter={(filter) => this.selectFilter(filter)} />
+        <Portfolio projects={this.state.projects} />
+      </div>
+    )
+  }
+}
