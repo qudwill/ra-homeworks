24104e8ea4c9a8481a7069c687a616da488a6e2d
Vladimir Isaev <info@isaev.in>
2017-08-05 11:40:15 +0300

    Решена задача dropdown

--- state/dropdown/js/App.js
+++ state/dropdown/js/App.js
@@ -1,37 +1,46 @@
-'use strict'
+'use strict';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     
     this.state = {
-      current: this.props.filters[0],
-      projects: this.props.projects
+      active: this.props.options[0],
+      open: false
     }
   }
-    
-  selectFilter(filter) {
-    let projects = this.props.projects;
-    
-    if (filter !== 'All') {
-      projects = projects.filter(project => project.category === filter);
-    }
-    
+  
+  handleChange(option) {
     this.setState({
-      current: filter,
-      projects: projects
+      active: option
+    });
+  }
+  
+  toggleOpen() {
+    this.setState({
+      open: !this.state.open
     });
   }
   
   render() {
     return (
-      <div>
-        <Toolbar
-          filters={this.props.filters}
-          selected={this.state.current}
-          onSelectFilter={(filter) => this.selectFilter(filter)} />
-        <Portfolio projects={this.state.projects} />
+      <div className="container">
+        <div className={`dropdown-wrapper ${this.state.open ? "open" : ""}`} >
+          <button className={"btn"} onClick={() => this.toggleOpen()} >
+            <span>Account Settings</span>
+            <i className="material-icons">public</i>
+          </button>
+          <ul className="dropdown">
+            {this.props.options.map((option, i) => (
+              <li
+                className={option === this.state.active ? "active" : ""}
+                onClick={() => this.handleChange(option)} >
+                  <a href="#">{option}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-    )
+    );
   }
 }
