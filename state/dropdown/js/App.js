7e49a17a0f11515628cddfd5dbb4e09cb8ec46fa
Vladimir Isaev <info@isaev.in>
2017-08-05 11:38:32 +0300

    Решена задача с фильтрами

--- state/dropdown/js/App.js
+++ state/dropdown/js/App.js
@@ -1,42 +1,37 @@
-var App = React.createClass({
-  defaultProps: {
-    options: []
-  },
-  getInitialState: function () {
-    return {
-      active: this.props.options[0],
-      open: false
-    };
-  },
-  handleChange: function (option) {
-    this.setState({
-      active: option
-    });
-  },
-  toggleOpen: function () {
-    this.setState({
-      open: !this.state.open
-    });
-  },
-  render: function () {
-    return (
-      <div className="container">
-        <div className={`dropdown-wrapper ${this.state.open ? "open" : ""}`} >
-          <button className={"btn"} onClick={this.toggleOpen} >
-            <span>Account Settings</span>
-            <i className="material-icons">public</i>
-          </button>
-          <ul className="dropdown">
-            {this.props.options.map((option, i) => (
-              <li
-                className={option === this.state.active ? "active" : ""}
-                onClick={() => this.handleChange(option)} >
-                  <a href="#">{option}</a>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
-    );
+'use strict'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      current: this.props.filters[0],
+      projects: this.props.projects
+    }
   }
-});
+    
+  selectFilter(filter) {
+    let projects = this.props.projects;
+    
+    if (filter !== 'All') {
+      projects = projects.filter(project => project.category === filter);
+    }
+    
+    this.setState({
+      current: filter,
+      projects: projects
+    });
+  }
+  
+  render() {
+    return (
+      <div>
+        <Toolbar
+          filters={this.props.filters}
+          selected={this.state.current}
+          onSelectFilter={(filter) => this.selectFilter(filter)} />
+        <Portfolio projects={this.state.projects} />
+      </div>
+    )
+  }
+}
