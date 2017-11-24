baf25f03c5afd6f6239ce24d8f771cf822e72019
Vladimir Isaev <qudwill@gmail.com>
2017-11-25 00:13:43 +0300

    Решена задача с поисковой строки библиотеки

--- ctrl-component/library/js/SearchBox.js
+++ ctrl-component/library/js/SearchBox.js
@@ -1,5 +1,7 @@
-const SearchBox = () => {
-    return (
-        <input type="text" placeholder="Поиск по названию или автору"/>
-    );
+'use strict';
+
+const SearchBox = ({value, filterBooks}) => {
+	return (
+		<input type="text" placeholder="Поиск по названию или автору" value={value} onChange={(event) => filterBooks(event.currentTarget.value)}/>
+	);
 };
