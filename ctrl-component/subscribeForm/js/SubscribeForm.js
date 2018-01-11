c0a48f5f1ae1780156dd134c25443b95b76f4991
Vladimir Isaev <qudwill@gmail.com>
2018-01-11 18:57:17 +0300

    bind перенесен в конструктор

--- ctrl-component/subscribeForm/js/SubscribeForm.js
+++ ctrl-component/subscribeForm/js/SubscribeForm.js
@@ -6,6 +6,8 @@ class SubscribeForm extends React.Component {
 			email: '',
 			formClassName: ''
 		};
+
+		this.changeEmail = this.changeEmail.bind(this);
 	};
 
 	changeEmail(evt) {
@@ -39,7 +41,7 @@ class SubscribeForm extends React.Component {
     			<h4 className="form-title">Подписаться:</h4>
     			<div className="form-group">
     	  		<label htmlFor="input-email" className="sr-only">Email</label>
-    	  		<input type="email" id="input-email" placeholder="Email" className="form-control" value={ this.state.email } onChange={ this.changeEmail.bind(this) }/>
+    	  		<input type="email" id="input-email" placeholder="Email" className="form-control" value={ this.state.email } onChange={ this.changeEmail }/>
     	  		<div className="form-error">Пожалуйста, проверьте корректность адреса электронной почты</div>
     	  		<button type="submit" className="form-next">
     	  	  	<i className="material-icons">keyboard_arrow_right</i>
