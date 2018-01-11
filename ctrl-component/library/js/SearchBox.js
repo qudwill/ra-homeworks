af69bae000c986776b7c18b6d06f53acefb62d34
Vladimir Isaev <qudwill@gmail.com>
2018-01-11 18:58:01 +0300

    event заменен на evt в library

--- ctrl-component/library/js/SearchBox.js
+++ ctrl-component/library/js/SearchBox.js
@@ -2,6 +2,6 @@
 
 const SearchBox = ({value, filterBooks}) => {
 	return (
-		<input type="text" placeholder="Поиск по названию или автору" value={value} onChange={(event) => filterBooks(event.currentTarget.value)}/>
+		<input type="text" placeholder="Поиск по названию или автору" value={value} onChange={(evt) => filterBooks(evt.currentTarget.value)}/>
 	);
 };
