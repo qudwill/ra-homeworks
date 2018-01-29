2c59ecf8eabe98a694e3711fbda50ddf9b53e293
Vladimir Isaev <qudwill@gmail.com>
2018-01-29 11:59:03 +0300

    Исправлено выравнивание

--- ctrl-component/library/js/SearchBox.js
+++ ctrl-component/library/js/SearchBox.js
@@ -2,6 +2,11 @@
 
 const SearchBox = ({value, filterBooks}) => {
 	return (
-		<input type="text" placeholder="Поиск по названию или автору" value={value} onChange={(evt) => filterBooks(evt.currentTarget.value)}/>
+		<input
+			type="text"
+			placeholder="Поиск по названию или автору"
+			value={value}
+			onChange={(evt) => filterBooks(evt.currentTarget.value)}
+		/>
 	);
 };
