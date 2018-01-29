5b00b9a21659059e2b55f542a09b4d6f9e28e5b3
Vladimir Isaev <qudwill@gmail.com>
2018-01-29 11:55:47 +0300

    Деструктурирован this.state в методе render()

--- ctrl-component/subscribeForm/js/SubscribeForm.js
+++ ctrl-component/subscribeForm/js/SubscribeForm.js
@@ -33,13 +33,15 @@ class SubscribeForm extends React.Component {
 	}
 
 	render() {
+		const { state } = this;
+		
 		return (
 			<div className="subscribe__form">
-  			<form className={`form form--subscribe ${this.state.formClassName}`}>
+  			<form className={`form form--subscribe ${state.formClassName}`}>
     			<h4 className="form-title">Подписаться:</h4>
     			<div className="form-group">
     	  		<label htmlFor="input-email" className="sr-only">Email</label>
-    	  		<input type="email" id="input-email" placeholder="Email" className="form-control" value={ this.state.email } onChange={ this.changeEmail }/>
+    	  		<input type="email" id="input-email" placeholder="Email" className="form-control" value={ state.email } onChange={ this.changeEmail }/>
     	  		<div className="form-error">Пожалуйста, проверьте корректность адреса электронной почты</div>
     	  		<button type="submit" className="form-next">
     	  	  	<i className="material-icons">keyboard_arrow_right</i>
